2f5cdf2f2a5a100dfa342a2ae38e157f4de950fb
jeonhyeonseon <good_pretty@naver.com>
2026-02-20 21:01:01 +0900

    chore: initial commit

---
+++ src/main/java/com/digitaldetox/digital_detox/DigitalDetoxApplication.java
@@ -0,0 +1,13 @@
+package com.digitaldetox.digital_detox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalDetoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalDetoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/digitaldetox/digital_detox/DigitalDetoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitaldetox.digital_detox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalDetoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
