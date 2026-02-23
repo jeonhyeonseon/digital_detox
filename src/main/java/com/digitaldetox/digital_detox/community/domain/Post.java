8df173ce98a50bfca5736c5d273a7d1678ebe338
jeonhyeonseon <good_pretty@naver.com>
2026-02-23 14:47:14 +0900

    feat: Post 엔터티 생성

---
+++ src/main/java/com/digitaldetox/digital_detox/community/domain/Post.java
@@ -0,0 +1,36 @@
+package com.digitaldetox.digital_detox.community.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tbl_post")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    private Long memberId; // 작성자
+
+    @Enumerated(EnumType.STRING)
+    private PostCategory postCategory;
+
+    private String title;
+
+    private String content;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+}
