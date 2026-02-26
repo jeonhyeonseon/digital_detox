1594d1df53529d643bd6f03f8e5ab2d790569834
jeonhyeonseon <good_pretty@naver.com>
2026-02-26 18:02:16 +0900

    feat: 시간 어노테이션 추가

--- src/main/java/com/digitaldetox/digital_detox/DigitalDetoxApplication.java
+++ src/main/java/com/digitaldetox/digital_detox/DigitalDetoxApplication.java
@@ -2,8 +2,10 @@ package com.digitaldetox.digital_detox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DigitalDetoxApplication {
 
     public static void main(String[] args) {
