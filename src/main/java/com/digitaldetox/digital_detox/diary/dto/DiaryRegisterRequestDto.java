fca9104a9613c1d74267e76f6c5ca5d00be93408
jeonhyeonseon <good_pretty@naver.com>
2026-03-09 19:39:32 +0900

    udpate: diary 등록 - 필드 추가 #17

--- src/main/java/com/digitaldetox/digital_detox/diary/dto/DiaryRegisterRequestDto.java
+++ src/main/java/com/digitaldetox/digital_detox/diary/dto/DiaryRegisterRequestDto.java
@@ -6,17 +6,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DiaryRegisterRequestDto {
 
+    private Long memberId;
+    private LocalDate diaryDate;
     private Mood mood;
     private int screenTime;
     private String content;
 
     public Diary toDiary() {
         return Diary.builder()
+                .memberId(memberId)
+                .diaryDate(this.diaryDate)
                 .mood(this.mood)
                 .screenTime(this.screenTime)
                 .content(this.content)
