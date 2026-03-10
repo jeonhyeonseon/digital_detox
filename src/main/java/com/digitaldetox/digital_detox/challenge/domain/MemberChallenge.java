19620a6abe52ba7cda9a1492f801143cee00b36b
jeonhyeonseon <good_pretty@naver.com>
2026-03-10 19:44:47 +0900

    feat: MemberChallenge 엔터티 생성 #26

---
+++ src/main/java/com/digitaldetox/digital_detox/challenge/domain/MemberChallenge.java
@@ -0,0 +1,32 @@
+package com.digitaldetox.digital_detox.challenge.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tbl_member_challenge")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberChallenge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberChallengeId;
+
+    private Long memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+    private LocalDate startDate;
+
+    @Enumerated(EnumType.STRING)
+    private MemberChallengeStatus memberChallengeStatus;
+
+}
